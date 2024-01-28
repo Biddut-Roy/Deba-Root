942649d34ef0ef28d30954e19b9ab039e1482fda
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-28 16:04:00 +0600

    Footer section complete

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Component/Navbar&Footer/Navbar";
+import Footer from "./Component/Navbar&Footer/Footer";
 
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
             <div className="block w-full max-w-screen-xl px-4 py-3 mx-auto text-white">
                 <Outlet />
             </div>
+            <Footer />
         </div>
     );
 };
