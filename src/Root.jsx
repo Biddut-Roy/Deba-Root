e0459bc08d48fef96a75815aa1ef9004b1e27240
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-28 11:34:12 +0600

    navbar section complete

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Component/Navbar&Footer/Navbar";
 
 
 const Root = () => {
     return (
         <div>
-            <Outlet />
+            <Navbar />
+            <div className="block w-full max-w-screen-xl px-4 py-3 mx-auto text-white">
+                <Outlet />
+            </div>
         </div>
     );
 };
